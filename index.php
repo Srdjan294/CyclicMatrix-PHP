61ca40e22952069df7dc518351f6fd2180341cb9
Srdjan294 <srdjanfilipovic991@gmail.com>
2021-05-23 13:42:47 +0200

    Trying to make 4*4 matrix in php

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    
+    $m = 4; 
+    $n = 4; 
+    $matrix[$i][$j];
+
+    for($i = 0; $i < $m; $i++){
+        for($j = 0; $j < $n; $j++){
+            echo ($matrix[$i][$j]." ");
+        }
+        echo(" ");
+    }
+
+    ?>
+</body>
+</html>
