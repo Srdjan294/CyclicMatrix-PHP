6893ff2e5021557bb54387c9c4cb2d3b9d8005bf
Srdjan294 <srdjanfilipovic991@gmail.com>
2021-05-24 13:32:47 +0200

    Form for input rows and columns, todo make matrix spiral

--- index.php
+++ index.php
@@ -7,10 +7,37 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
+
+    <form action="" method="post">
+        <label for="rows">Rows:</label>
+        <input type="text" id="rows" name="rows"><br><br>
+        <label for="columns">Columns:</label>
+        <input type="text" id="columns" name="columns"><br><br>
+        <input type="submit" value="Submit">
+    </form>
+
+    <br>
+
     <?php
     
-    $rows = 4; 
-    $columns = 4; 
+    if (isset($_POST["rows"])){
+        $rows = $_POST["rows"];
+    } else {
+        $rows = null;
+        echo "Enter rows.";
+    }?>
+
+    <br>
+
+    <?php
+    
+    if (isset($_POST["columns"])){
+        $columns = $_POST["columns"];
+    } else {
+        $columns = null;
+        echo "Enter columns.";
+    }
+    
     $k = 1;
     $matrix = array(array()); ?>
     
@@ -29,8 +56,6 @@
         <?php } ?> 
         
             </tr>
-        
-        <?php echo '<br>'; ?>
         
         <?php } ?>
         
