fbfe2a8179a2a307f2303b8fe5ca82fd8e8e384a
Srdjan294 <srdjanfilipovic991@gmail.com>
2021-05-24 20:26:53 +0200

    Cyclic matrix done

--- index.php
+++ index.php
@@ -18,48 +18,7 @@
 
     <br>
 
-    <?php
-    
-    if (isset($_POST["rows"])){
-        $rows = $_POST["rows"];
-    } else {
-        $rows = null;
-        echo "Enter rows.";
-    }?>
-
-    <br>
-
-    <?php
-    
-    if (isset($_POST["columns"])){
-        $columns = $_POST["columns"];
-    } else {
-        $columns = null;
-        echo "Enter columns.";
-    }
-    
-    $k = 1;
-    $matrix = array(array()); ?>
-    
-    <table>
-    
-        <?php for($i = 0; $i < $rows; $i++){ ?>
-        
-            <tr>
-        
-        <?php for($j = 0; $j < $columns; $j++){ ?>
-        
-            <td>
-            <?php  echo $matrix[$i][$j] = $k++; ?>
-            </td>
-        
-        <?php } ?> 
-        
-            </tr>
-        
-        <?php } ?>
-        
-    </table>    
+   <?php include 'cyclicmatrix.php'; ?>
 
 </body>
 </html>
